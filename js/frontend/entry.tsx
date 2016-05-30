5b00a18fcc3904549e8fdb40d44e79504fcbe37f
Andreas Ehrencrona <andreas.ehrencrona@velik.it>
2016-05-30 08:51:07 +0200

    make errors visible

--- js/frontend/entry.tsx
+++ js/frontend/entry.tsx
@@ -19,6 +19,11 @@ let React = { createElement: createElement }
 
 let lang = getLanguage()
 
+let handleException = (e) => {
+    alert('Error while saving.')
+    console.log(e)
+}
+
 xr.get(`/api/${lang}/corpus`)
 .then((xhr) => {
     var element = document.getElementById('react-root');
@@ -26,10 +31,12 @@ xr.get(`/api/${lang}/corpus`)
 
     corpus.sentences.onChange = (sentence: Sentence) => {
         xr.put(`/api/${lang}/sentence/` + sentence.getId(), sentence.toJson())        
+            .catch(handleException)
     }
 
     corpus.sentences.onDelete = (sentence: Sentence) => {
         xr.del(`/api/${lang}/sentence/` + sentence.getId())        
+            .catch(handleException)
     }
 
     corpus.sentences.onAdd = (sentence: Sentence) => {
@@ -37,25 +44,30 @@ xr.get(`/api/${lang}/corpus`)
             .then((res) => {
                 corpus.sentences.changeId(sentence.id, res.data.id)
             })
+            .catch(handleException)
     }
     
     corpus.words.onAdd = (word: Word) => {
         xr.post(`/api/${lang}/word/` + word.jp , {
             inflection: (word instanceof InflectedWord ? word.inflection.getId() : '') })
+            .catch(handleException)
     }
 
     corpus.words.onChangeInflection = (word: InflectedWord) => {
         xr.put(`/api/${lang}/word/` + word.getId() + '/inflection/' + word.inflection.getId(), {})
+            .catch(handleException)
     }
 
     corpus.facts.onMove = (fact: Fact, pos: number) => {
         xr.put(`/api/${lang}/fact/${pos}/${ fact.getId() }`, {})
+            .catch(handleException)
     }
 
     corpus.facts.onAdd = (fact: Fact) => {
         // new words are caught through the words listener
         if (fact instanceof InflectionFact) {
             xr.post(`/api/${lang}/fact/${ fact.getId() }`, {})
+                .catch(handleException)
         }
     }
 
