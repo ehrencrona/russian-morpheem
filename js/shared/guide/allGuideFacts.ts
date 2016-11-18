487c902d2707bef9563f847534cc3b37dddcf66a
Andreas Ehrencrona <andreas.ehrencrona@velik.it>
2016-11-18 12:07:31 +0100

    fact search was broken

--- js/shared/guide/allGuideFacts.ts
+++ js/shared/guide/allGuideFacts.ts
@@ -6,6 +6,6 @@ import WORD_FORMS from '../inflection/WordForms'
 
 export default function allGuideFacts(corpus: Corpus) {
     return corpus.facts.facts
-        .concat(Object.keys(WORD_FORMS).filter(f => !corpus.facts.get(f)).map(k => FORMS[k]))
+        .concat(Object.keys(WORD_FORMS).filter(f => !corpus.facts.get(f)).map(k => WORD_FORMS[k]))
         .concat(Object.keys(FORMS).filter(f => !corpus.facts.get(f) && !WORD_FORMS[f]).map(k => FORMS[k]))
 }
